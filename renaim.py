2d5caba3b5bc8b5f9c83aa6f00329be042b9d3db
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2023-05-08 14:30:22 -0500

    Update renaim.py

--- renaim.py
+++ renaim.py
@@ -4,11 +4,19 @@ def rename_files():
     path = os.getcwd()
     file_list = os.listdir(path)
 
+    # Initialize a counter for the new sequence
+    count = 1
+
+    # Sort the file_list to ensure correct ordering
+    file_list.sort()
+
     for file in file_list:
         if file.endswith(".jpg"):
             split_name = file.split('-')
             if len(split_name) > 1:
-                number = split_name[0]
+                # Replace the original number with the counter value
+                number = count
+                count += 1
                 suffix = split_name[1].split('.')[0]
                 new_name = f'{suffix}-{number}.jpg'
                 os.rename(file, new_name)
